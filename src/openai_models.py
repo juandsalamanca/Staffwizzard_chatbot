99da8eb09132a32ee9e1f59e39bf3a17f148b3f5
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-02-28 10:58:38 -0500

    Update openai_models.py

--- src/openai_models.py
+++ src/openai_models.py
@@ -2,16 +2,10 @@
 from openai import AzureOpenAI
 def employee_info_chatbot(model_engine, added_messages, info):
 
-    # Replace with your desired model
     client = AzureOpenAI(api_key="4d165b8e531c49ec8ac0c256aa73d4bd", api_version="2024-02-01", azure_endpoint = "https://azo-iknow-ku-eus-01.openai.azure.com/")
     #client = OpenAI()
 
     # Construct the prompt using formatted strings
-
-    prompt = f"""You need to answer what the user asks based on the following information: {info}
-                 the user says: {user_prompt}
-              """
-
     system_prompt = f"""You're a useful assistant that helps employees find information about the company,
                         their status in the company or make requests. I fthey make a request that is not just
                         finding information and that requires additional information for it to be processed
@@ -35,22 +29,14 @@ def employee_info_chatbot(model_engine, added_messages, info):
 
     response = response.choices[0].message.content
 
-    #print(response)
-    #print("Adult Rewrite Response " + str(count))
     return response
 
 def employee_sensitive_info_chatbot(model_engine, added_messages, info):
 
-    # Replace with your desired model
     client = AzureOpenAI(api_key="4d165b8e531c49ec8ac0c256aa73d4bd", api_version="2024-02-01", azure_endpoint = "https://azo-iknow-ku-eus-01.openai.azure.com/")
     #client = OpenAI()
 
     # Construct the prompt using formatted strings
-
-    prompt = f"""You need to answer what the user asks based on the following information: {info}
-                 the user says: {user_prompt}
-              """
-
     messages = [
     {"role": "system",
      "content": "You're a useful assistant that helps employees know if certain information about them is available for them to see. This could be 401k, PTO balance amounts etc"}
@@ -68,6 +54,4 @@ def employee_sensitive_info_chatbot(model_engine, added_messages, info):
 
     response = response.choices[0].message.content
 
-    #print(response)
-    #print("Adult Rewrite Response " + str(count))
     return response
