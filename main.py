8f955070207c1b2ea108fe8b134c9f0fc7e32665
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-03-03 14:26:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ if input:
     if sensitive:
         response = employee_sensitive_info_chatbot("gpt-4o-mini", st.session_state.memory, info)
         st.session_state.memory.append({"role": "assistant", "content": response})
+        response += " "
         response += sensitive_info
     else:
         response = employee_info_chatbot("gpt-4o-mini", st.session_state.memory, info)
